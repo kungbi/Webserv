4eb7593b690f8b063a1d0d519d994cfcd8f99079
kungbi <kipper12335@gmail.com>
2025-02-24 22:05:44 +0900

    Feat: requests 클래스 구현

---
+++ include/webserver/Requests.hpp
@@ -0,0 +1,22 @@
+#ifndef REQUESTS_HPP
+# define REQUESTS_HPP
+
+# include <iostream>
+# include "Request.hpp"
+# include <map>
+
+class Requests {
+private:
+	std::map<int, Request> requests_;
+
+public:
+	Requests();
+
+	void addRequest(Request& request);
+	size_t size() const;
+	bool isExist(int fd) const;
+	Request& getRequest(int fd);
+	void removeRequest(int fd);
+};
+
+#endif
---
+++ src/webserver/Requests.cpp
@@ -0,0 +1,35 @@
+#include "Requests.hpp"
+
+Requests::Requests() {}
+
+void Requests::addRequest(Request& request) {
+	if (this->isExist(request.getFd())) {
+		std::cerr << "Request with fd " << request.getFd() << " already exists" << std::endl;
+		return;
+	}
+	requests_[request.getFd()] = request;
+}
+
+size_t Requests::size() const {
+	return requests_.size();
+}
+
+bool Requests::isExist(int fd) const {
+	return requests_.find(fd) != requests_.end();
+}
+
+Request& Requests::getRequest(int fd) {
+	if (!this->isExist(fd)) {
+		std::cerr << "Request with fd " << fd << " does not exist" << std::endl;
+		throw std::runtime_error("Request with fd does not exist");
+	}
+	return requests_[fd];
+}
+
+void Requests::removeRequest(int fd) {
+	if (!this->isExist(fd)) {
+		std::cerr << "Request with fd " << fd << " does not exist" << std::endl;
+		return;
+	}
+	requests_.erase(fd);
+}
