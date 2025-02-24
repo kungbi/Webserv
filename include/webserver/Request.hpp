59d2cbdd6908d6cb62a8ec3e0edb84ed78193a29
kungbi <kipper12335@gmail.com>
2025-02-24 20:58:26 +0900

    Feat: request 클래스 구현

---
+++ include/webserver/Request.hpp
@@ -0,0 +1,26 @@
+#ifndef REQUEST_HPP
+# define REQUEST_HPP
+
+# include <iostream>
+# include <string>
+
+class Request {
+private:
+	int fd_;
+
+	bool complete_;
+	std::string originalRequest_;
+	// 파싱된 내용들 추가
+
+	Request();
+
+public:
+	Request(int fd);
+
+	bool isComplete() const;
+	void appendData(const char* data, size_t length);
+
+	bool equalFd(int fd) const;
+};
+
+#endif
---
+++ src/webserver/Request.cpp
@@ -0,0 +1,18 @@
+#include "Request.hpp"
+
+Request::Request(int fd) : fd_(fd), complete_(false) {}
+
+bool Request::isComplete() const {
+	return complete_;
+}
+
+void Request::appendData(const char* data, size_t length) {
+	originalRequest_.append(data, length);
+
+	if (originalRequest_.find("\r\n\r\n") != std::string::npos)
+		complete_ = true;
+}
+
+bool Request::equalFd(int fd) const {
+	return fd_ == fd;
+}
